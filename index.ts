c09cdb59f3a66a426df9a4b4b9f646b8907b5e14
Timo Sand <timo.sand@iki.fi>
2024-10-11 18:37:57 +0300

    Add naive implementation of a weeks menu
    Signed-off-by: Timo Sand <timo.sand@iki.fi>

--- index.ts
+++ index.ts
@@ -1,1 +1,40 @@
-console.log('Hello via Bun!');
+const recipes = [
+  'Kanaton curry keitto',
+  'Sitruunainen hernepasta',
+  'Makaronilaatikko',
+  'Hernekeitto',
+  'Kaalilaatikko',
+  'Myskikurpitsakeitto',
+  'Tacopelti',
+  'Heroinipasta',
+  'Vihapullat ja Perunamuusi',
+  'Nugetit ja Perunamuusi',
+  'Itkutofu',
+  'Tikka masala tofulla',
+  'Nyhtökaurakäristys',
+  'Tofupuikot ja hernetahna',
+  'Tofunugetit',
+  'Uunipyttipannu',
+  'Soijarouhebolognese',
+  'Nyhtökaura-gnocchivuoka',
+  'Pinaattikeitto',
+  'Pitsaperjantai',
+  'Fajitas',
+  'Tofunuudelit',
+  'Vegaaninen butter tofu',
+  'Quornkiusaus',
+  'Quornpihvejä ja kermaperunoita',
+  'Lasagne',
+  'Makkarakeitto',
+  'Makkarakastike ja perunaa',
+];
+
+const randomRecipe = () => recipes[Math.floor(Math.random() * recipes.length)];
+
+const weeksRecipes = new Set();
+
+while (weeksRecipes.size < 5) {
+  weeksRecipes.add(randomRecipe());
+}
+
+console.log(`This week's recipes are: ${Array.from(weeksRecipes).join(', ')}`);
